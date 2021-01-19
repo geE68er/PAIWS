4d023f08bd18cf8e47b4a362ec974bc438dde126
rupertgehrlein <75689560+rupertgehrlein@users.noreply.github.com>
2021-01-19 12:11:53 +0100

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <navbar class="navbar" />
+    <div id="content">
+      <router-view class="main"/>
+    </div>
+    <footer id="footer">
+      Copyright &copy; Gehrlein, Großmann, Kahl, Omerbegovic, Steckelbroeck
+    </footer>
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+export default {
+  components: { Navbar },
+  name: 'app'
+}
+</script>
+
+<style>
+
+  #app {
+    font-family: "Helvetica", sans-serif;
+    position: relative;
+    min-height: 100vh;
+    cursor: default;
+  }
+
+  #footer {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 2rem;
+    background-color: darkgray;
+    padding-top: 0.5rem;
+    text-align: right;
+  }
+
+  #content {
+    padding-bottom: 2rem;
+    max-width: 70%;
+    margin: auto;
+    text-align: left;
+  }
+  
+  
+</style>
