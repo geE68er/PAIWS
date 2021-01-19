69e52dd1c6603410246ed695888108c568f21cee
rupertgehrlein <75689560+rupertgehrlein@users.noreply.github.com>
2021-01-19 12:12:24 +0100

    Create Register.vue

---
+++ src/components/Register.vue
@@ -0,0 +1,24 @@
+<template>
+    <form action="register" class="register">
+        <table>
+            <tr><td>Vorname*: </td><td><input type="text" placeholder="Vorname" name="prename"></td></tr><br>
+            <tr><td>Nachname*: </td><td><input type="text" placeholder="Nachname" name="lastname"></td></tr><br>
+            <tr><td>E-Mail*: </td><td><input type="text" placeholder="E-Mail" name="email"></td></tr><br>
+            <tr><td>Passwort*: </td><td><input type="password" placeholder="Passwort" name="psw"></td></tr><br>
+            <tr><td>Passwort bestätigen*: </td><td><input type="password" placeholder="Passwort" name="psw"></td></tr><br>
+        </table>
+        <button type="submit">Registrieren</button>
+    </form>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+    td {
+        text-align: left;
+    }
+</style>
