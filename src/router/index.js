48c728e406d4bca9763cfcf8ab379c0261719b8c
rupertgehrlein <75689560+rupertgehrlein@users.noreply.github.com>
2021-01-19 12:12:30 +0100

    Create index.js

---
+++ src/router/index.js
@@ -0,0 +1,47 @@
+import Vue from "vue";
+import Router from "vue-router";
+import Homepage from "../components/Homepage";
+import Account from "../components/Account";
+import Contact from "../components/Contact";
+import Friends from "../components/Friends";
+import Login from "../components/Login";
+import Register from "../components/Register";
+
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: "/",
+      name: "Homepage",
+      component: Homepage
+    },
+    {
+      path: "/friends",
+      name: "Friends",
+      props: true,
+      component: Friends
+    },
+    {
+      path: "/contact",
+      name: "Contact",
+      component: Contact
+    },
+    {
+      path: "/account",
+      name: "Account",
+      component: Account
+    },
+    {
+      path: "/login",
+      name: "Login",
+      component: Login
+    },
+    {
+      path: "/register",
+      name: "Register",
+      component: Register
+    }
+  ]
+})
