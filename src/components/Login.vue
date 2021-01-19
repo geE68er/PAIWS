b5375559cc3a69075fdf47d23b4bd9546a6ac1e8
rupertgehrlein <75689560+rupertgehrlein@users.noreply.github.com>
2021-01-19 12:12:20 +0100

    Create Login.vue

---
+++ src/components/Login.vue
@@ -0,0 +1,21 @@
+<template>
+    <form action="login" class="login">
+        <table>
+            <tr><td>E-Mail*: </td><td><input type="text" placeholder="E-Mail" name="email"></td></tr><br>
+            <tr><td>Passwort*: </td><td><input type="text" placeholder="Passwort" name="psw"></td></tr><br>
+        </table>
+        <button type="submit">Login</button>
+    </form>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+    td {
+        text-align: left;
+    }
+</style>
