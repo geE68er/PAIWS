b51c6e9f7d5f5885144df6ceae62352092870224
rupertgehrlein <75689560+rupertgehrlein@users.noreply.github.com>
2021-01-19 12:12:26 +0100

    Create Search.vue

---
+++ src/components/Search.vue
@@ -0,0 +1,12 @@
+<template>
+    <form action="search" class="search">
+        <input type="text" placeholder="Search...">
+        <button type="submit"><font-awesome-icon :icon="['fas', 'search']" /></button>
+    </form>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
