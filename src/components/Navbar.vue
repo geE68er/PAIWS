c6954784a0b4282ce8081bf2d5c9d0908661c243
rupertgehrlein <75689560+rupertgehrlein@users.noreply.github.com>
2021-01-20 11:11:10 +0100

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,88 +1,116 @@
 <template>
-    <div id="navigation">
-        <ul>
-            <li><router-link to="/" tag="li"><font-awesome-icon :icon="['fas', 'home']" />Home</router-link></li>
-            <li><router-link to="friends" tag="li"><font-awesome-icon :icon="['fas', 'users']" />Friends</router-link></li>
-            <li><router-link to="account" tag="li"><font-awesome-icon :icon="['fas', 'user-circle']" />Account</router-link></li>
-            <li><router-link to="contact" tag="li"><font-awesome-icon :icon="['fas', 'phone-alt']" />Contact</router-link></li>
-            <li><search /></li>
-            <li><button id="register"><router-link to="register" tag="li_register">Registrieren</router-link></button></li>
-            <li><button id="login"><router-link to="login" tag="li_login">Anmelden</router-link></button></li>
-        </ul>
-    </div>
+  <div id="navigation">
+    <ul>
+      <li>
+        <router-link to="/" tag="li"
+          ><font-awesome-icon :icon="['fas', 'home']" />Home</router-link
+        >
+      </li>
+      <li>
+        <router-link to="friends" tag="li"
+          ><font-awesome-icon :icon="['fas', 'users']" />Friends</router-link
+        >
+      </li>
+      <li>
+        <router-link to="account" tag="li"
+          ><font-awesome-icon
+            :icon="['fas', 'user-circle']"
+          />Account</router-link
+        >
+      </li>
+      <li>
+        <router-link to="contact" tag="li"
+          ><font-awesome-icon
+            :icon="['fas', 'phone-alt']"
+          />Contact</router-link
+        >
+      </li>
+      <li><search /></li>
+      <li>
+        <button id="register">
+          <router-link to="register" tag="li_register"
+            >Registrieren</router-link
+          >
+        </button>
+      </li>
+      <li>
+        <button id="login">
+          <router-link to="login" tag="li_login">Anmelden</router-link>
+        </button>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-import Search from './Search.vue'
+import Search from "./Search.vue";
 export default {
-  components: { Search },
-  
-}
+  components: { Search }
+};
 </script>
 <style>
-    #navigation {
-    font-size: 30px;
-    display: flex;
-    width: 100%;
-    margin-bottom: 50px;
-    }
-    
-    #navigation ul {
-    display: flex;
-    list-style: none;
-    padding: 0;
-    margin: 0 20px 0 0;
-    }
+#navigation {
+  font-size: 30px;
+  display: flex;
+  width: 100%;
+  margin-bottom: 50px;
+}
 
-    li {
-    font-size: 2rem;
-    padding: 10px 25px 10px 10px;
-    text-decoration: none;
-    }
+#navigation ul {
+  display: flex;
+  list-style: none;
+  padding: 0;
+  margin: 0 20px 0 0;
+}
 
-    .search {
-    position: relative;
-    width: 100%;
-    max-width: 400px;
-    padding-left: 80px;
-    }
+li {
+  font-size: 2rem;
+  padding: 10px 25px 10px 10px;
+  text-decoration: none;
+}
 
-    .search input {
-        border: none;
-        outline: none;
-        width: calc(100% - 80px);
-        padding: 15px 60px 15px 20px;
-        margin: 0;
-        border-radius: 20px;
-        background-color: #efefef;
-        font-family: "Segoe UI", Tahoma;
-        font-size: 1rem;
-    }
+.search {
+  position: relative;
+  width: 100%;
+  max-width: 400px;
+  padding-left: 80px;
+}
 
-    .search button {
-        position: absolute;
-        font-size: 20px;
-        margin-right: 30px;
-        padding: 15px 15px;
-        margin-left: 5px;
-        border: none;
-        background-color: #ffffff;
-    }
+.search input {
+  border: none;
+  outline: none;
+  width: calc(100% - 80px);
+  padding: 15px 60px 15px 20px;
+  margin: 0;
+  border-radius: 20px;
+  background-color: #efefef;
+  font-family: "Segoe UI", Tahoma;
+  font-size: 1rem;
+}
 
-    #register {
-        margin-left: 350px;
-        background-color: chartreuse;
-        border-radius: 10px;
-        font-size: 1.2rem;
-        text-decoration: none;
-        font-weight: bold;  
-    }
+.search button {
+  position: absolute;
+  font-size: 20px;
+  margin-right: 30px;
+  padding: 15px 15px;
+  margin-left: 5px;
+  border: none;
+  background-color: #ffffff;
+}
 
-    #login {
-        background-color: rgb(224, 26, 12);
-        border-radius: 10px;
-        font-size: 1.2rem;
-        font-weight: bold; 
-    }
+#register {
+  margin-left: 350px;
+  background-color: chartreuse;
+  border-radius: 10px;
+  font-size: 1.2rem;
+  text-decoration: none;
+  font-weight: bold;
+}
 
+#login {
+  background-color: rgb(224, 26, 12);
+  border-radius: 10px;
+  font-size: 1.2rem;
+  font-weight: bold;
+}
 </style>
