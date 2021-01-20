9473db0f09f9d9423d10b1a54120b62b09792b1f
rupertgehrlein <75689560+rupertgehrlein@users.noreply.github.com>
2021-01-20 20:30:55 +0100

    Create About.vue

---
+++ src/components/About.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+  <h1>Copyright by:</h1><br>
+  <h2>Damir</h2><br>
+  <h2>Gabriel</h2><br>
+  <h2>Jan</h2><br>
+  <h2>Nils</h2><br>
+  <h2>Rupert</h2><br>
+
+  
+</div>
+</template>
+
+<script>
+export default {};
+</script>
