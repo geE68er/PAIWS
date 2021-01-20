c9d61b879a14dfb53202e855a78c60cfc5245be2
rupertgehrlein <75689560+rupertgehrlein@users.noreply.github.com>
2021-01-20 20:30:58 +0100

    Update Contact.vue

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,5 +1,14 @@
 <template>
-  <h1>Contact</h1>
+  <div>
+    <h1><font-awesome-icon :icon="['fas', 'phone-alt']"/> 555 - NASE</h1>
+    <h1><font-awesome-icon :icon="['fas', 'at']"/> test@test.de</h1>
+    <h1><font-awesome-icon :icon="['fas', 'phone-alt']"/> 555 - NASE</h1>
+    <h1><font-awesome-icon :icon="['fas', 'at']"/> test@test.de</h1>
+    <h1><font-awesome-icon :icon="['fas', 'phone-alt']"/> 555 - NASE</h1>
+    <h1><font-awesome-icon :icon="['fas', 'at']"/> test@test.de</h1>
+    <h1><font-awesome-icon :icon="['fas', 'phone-alt']"/> 555 - NASE</h1>
+    <h1><font-awesome-icon :icon="['fas', 'at']"/> test@test.de</h1>
+  </div>
 </template>
 
 <script>
