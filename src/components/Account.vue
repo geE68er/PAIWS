c0835e2daee403e2bf65c463777b4bda685d2f7d
rupertgehrlein <75689560+rupertgehrlein@users.noreply.github.com>
2021-01-20 21:05:46 +0100

    Update Account.vue

--- src/components/Account.vue
+++ src/components/Account.vue
@@ -1,5 +1,10 @@
 <template>
-  <h1>Account</h1>
+  <div>
+    <h1>It's me...</h1> <br>
+    <img src="https://nintendo-online.de/upload/images/2020/03/30/5578102955e8201e2589853-57494356.jpg" height="400px">
+
+  </div>
+  
 </template>
 
 <script>
