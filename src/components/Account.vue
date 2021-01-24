6e9dcb3bdb60df76c6c3deffd7ffd8ef6fd52345
TheDaco <33269880+TheDaco@users.noreply.github.com>
2021-01-24 14:53:51 +0100

    1,1

--- src/components/Account.vue
+++ src/components/Account.vue
@@ -1,5 +1,26 @@
 <template>
-  <h1>Account</h1>
+<v-card height="150px">
+    <v-navigation-drawer
+      absolute
+      permanent
+      right>
+
+     <template v-slot:prepend>
+        <v-list-item two-line>
+          <v-list-item-avatar>
+            <img src="https://randomuser.me/api/portraits/women/81.jpg">
+          </v-list-item-avatar>
+
+          <v-list-item-content>
+            <v-list-item-title>Jane Smith</v-list-item-title>
+            <v-list-item-subtitle>Logged In</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </template>
+
+
+    </v-navigation-drawer>
+    </v-card>
 </template>
 
 <script>
