14b980d8360baa3b7f0dc4e9a095dd88e3b64802
rupertgehrlein <75689560+rupertgehrlein@users.noreply.github.com>
2021-01-25 15:25:49 +0100

    Update Login.vue

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,28 +1,34 @@
 <template>
-  <form action="login" class="login">
-    <table>
-      <tr>
-        <td>E-Mail*:</td>
-        <td><input type="text" placeholder="E-Mail" name="email" /></td>
-      </tr>
-      <br />
-      <tr>
-        <td>Passwort*:</td>
-        <td><input type="text" placeholder="Passwort" name="psw" /></td>
-      </tr>
-      <br />
-    </table>
+  <v-content>
+      <v-card width="500" class="mx-auto mt-9">
+        <v-card-title>Anmelden</v-card-title>
+        <v-card-text>
+          <v-text-field label="E-Mail" prepend-icon="mdi-account-circle"/>
+          <v-text-field 
+          label="Passwort" 
+          :type="showPassword ? 'text' : 'password'"
+          prepend-icon="mdi-lock"
+          :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+          @click:append="showPassword = !showPassword"/>
+        </v-card-text>
 
-    <button type="submit">Login</button>
-  </form>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-btn color="info"><router-link to="" tag="span">Anmelden</router-link></v-btn>
+          <v-spacer></v-spacer>
+          <v-btn color="red"><router-link to="/register" tag="span">Registrieren</router-link></v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-content>
 </template>
 
 <script>
-export default {};
-</script>
-
-<style>
-td {
-  text-align: left;
+export default {
+  data()
+  {
+    return{
+      showPassword:false
+    }
+  }
 }
-</style>
+</script>
