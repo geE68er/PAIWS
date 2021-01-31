d2d7ec31d7b01ef7e92f054fe2a55294a22e6a84
rupertgehrlein <75689560+rupertgehrlein@users.noreply.github.com>
2021-01-31 16:18:59 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,10 +5,11 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import vuetify from "./plugins/vuetify";
+import DatetimePicker from 'vuetify-datetime-picker';
 
 library.add(fas);
 Vue.component("font-awesome-icon", FontAwesomeIcon);
-
+Vue.use(DatetimePicker);
 Vue.config.productionTip = false;
 
 new Vue({
