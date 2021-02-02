73add049b35288b966ebdcbb8ba193162a0d997f
rupertgehrlein <75689560+rupertgehrlein@users.noreply.github.com>
2021-02-02 15:03:48 +0100

    Update Createevent.vue

--- src/components/Createevent.vue
+++ src/components/Createevent.vue
@@ -3,10 +3,10 @@
       <v-card max-width="700" max-height="1000" class="mx-auto mt-9">
         <v-card-title>Event erstellen</v-card-title>
         <v-card-text>
-          <v-text-field placeholder="Eventname" prepend-icon="mdi-account-circle"/>
-          <v-text-field placeholder="Ort" prepend-icon="mdi-map-marker"/>
-          <v-text-field placeholder="Beschreibung" prepend-icon="mdi-lead-pencil"/>
-          <v-datetime-picker v-model="formattedDatetime" date-format="dd.MM.yyyy" label="Datum und Uhrzeit">
+          <v-text-field required name="eventname" placeholder="Eventname" prepend-icon="mdi-account-circle"/>
+          <v-text-field required name="place" placeholder="Ort" prepend-icon="mdi-map-marker"/>
+          <v-text-field required name="description" placeholder="Beschreibung" prepend-icon="mdi-lead-pencil"/>
+          <v-datetime-picker required name="time" v-model="formattedDatetime" date-format="dd.MM.yyyy" label="Datum und Uhrzeit">
                   <template slot="dateIcon">
                     <v-icon>mdi-calendar</v-icon>
                   </template>
@@ -15,10 +15,9 @@
                   </template>
                 </v-datetime-picker>
         </v-card-text>
-
         <v-divider></v-divider>
         <v-card-actions>
-          <button class="submit" v-on:click="addEvent()"><router-link to="/events" tag="span">Erstellen</router-link></button>
+          <button class="submit" v-on:click="addEvent()"><router-link to="" tag="span">Erstellen</router-link></button>
         </v-card-actions>
       </v-card>
     </v-content>
@@ -28,7 +27,8 @@
 export default {
   methods: {
     addEvent:function(){
-      alert('Hallo')
+      let e_name = document.getElementsByName("eventname")[0].value;
+      alert(e_name);
     }
   }
 }
