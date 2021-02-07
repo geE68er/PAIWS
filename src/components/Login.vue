2f6a371e2538117f8616de77b97856d95d7f61a5
rupertgehrlein <75689560+rupertgehrlein@users.noreply.github.com>
2021-02-07 15:33:41 +0100

    Update Login.vue

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -24,11 +24,91 @@
 
 <script>
 export default {
+  methods: {
+					allRecords: function(){
+						axios.post('ajaxUser.php', {
+						    request: 1
+						})
+						.then(function (response) {
+							app.users = response.data;
+						})
+						.catch(function (error) {
+						    console.log(error);
+						});
+						
+					},
+					addRecord: function(){
+
+						if(this.username != '' && this.name != '' && this.email != ''){
+							axios.post('ajaxUser.php', {
+							    request: 2,
+							    username: this.username,
+							    name: this.name,
+							    email: this.email
+							})
+							.then(function (response) {
+
+								// Fetch records
+							    app.allRecords();
+
+							    // Empty values
+							    app.username = '';
+							    app.name = '';
+							    app.email = '';
+
+							    alert(response.data);
+							})
+							.catch(function (error) {
+							    console.log(error);
+							});
+						}else{
+							alert('Fill all fields.');
+						}
+									
+					},
+					updateRecord: function(index,id){
+
+						// Read value from Textbox
+						var name = this.users[index].name;
+						var email = this.users[index].email;
+
+						if(name !='' && email !=''){
+							axios.post('ajaxUser.php', {
+							    request: 3,
+							    id: id,
+							    name: name,
+							    email: email
+							})
+							.then(function (response) {
+								alert(response.data);
+							})
+							.catch(function (error) {
+							    console.log(error);
+							});
+						}
+					},
+					deleteRecord: function(index,id){
+						
+						axios.post('ajaxUser.php', {
+						    request: 4,
+						    id: id
+						})
+						.then(function (response) {
+
+							// Remove index from users
+						    app.users.splice(index, 1);
+						    alert(response.data);
+						})
+						.catch(function (error) {
+						    console.log(error);
+						});
+						  
+					},
   data()
   {
     return{
       showPassword:false
     }
   }
-}
+}}
 </script>
