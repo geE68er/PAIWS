76b8454ec02189532bf4cc9615448bcac7e4ab46
Sthacki <77539062+Sthacki@users.noreply.github.com>
2021-02-07 13:23:13 +0100

    Add files via upload

---
+++ event_insert.php
@@ -0,0 +1,28 @@
+<?php
+include(dbcon.php);
+$eName = $_POST["ename"];
+$euId = null // $_POST["uID"]; verknuepfung zur user tabelle, erstellender user automatsich einfügen!?
+$eDesc = $_POST["des"];
+$eLoc = $_POST["loc"];
+$erating = $_POST["rating"];
+$ecreationtime = $_POST["ecretime"];
+$edate = $_POST["edate"]
+$eduration = $_POST["dura"];
+$membCap = $_POST["memberC"];
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+$sql ="INSERT INTO event(eventID, eventname, eventuid, eventloc, eventabt, eventrtng, eventtme, eventdte, eventdrtn, eventtmbr, eventcp)
+VALUES(null,'$eName','$euId','$eLoc', '$eDesc', '$erating','$ecreationtime', '$edate', '$eduration', null, '$membCap');";
+if($conn->query($sql) == true){
+	echo "User hinzugefügt!";
+} else { "Hat nicht geklappt!" . $conn->error;
+}
+
+?>
---
+++ user_insert.php
@@ -0,0 +1,21 @@
+<?php
+// Skrit um sich mit der Datenbank zu verbinden
+include('dbcon.php');
+
+// Variablen gespeichert, die mit der POST-Methode übergeben werden
+$fname = $_POST["fname"]; //am besten benutzt ihr die Namen, wie sie 
+$nname = $_POST["nname"]; // in den eckigen Klammern stehen für eure 
+$pwo = $_POST["password"]; //übergabe
+$adr = $_POST["adr"];
+$mail = $_POST["mail"];
+
+// das sql insert mit den Attributnamen der Tabelle und den Werten
+$sql ="INSERT INTO user(uid, uname, ulname, uadrs,umail, upwd)
+VALUES(null,'$nname','$fname','$adr','$mail','$pwo');"; // die durch das Skript
+if($conn->query($sql) == true){ //eingefügt werden
+	echo "User hinzugefügt!";
+} else { "Hat nicht geklappt!" . $conn->error;
+}
+include('extr_user.php');
+
+?> 
