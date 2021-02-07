fb31f6664d05ce3c112254443771327e589a3912
rupertgehrlein <75689560+rupertgehrlein@users.noreply.github.com>
2021-02-07 14:31:46 +0100

    Update event_insert.php

--- event_insert.php
+++ event_insert.php
@@ -1,28 +1,20 @@
 <?php
-include(dbcon.php);
-$eName = $_POST["ename"];
-$euId = null // $_POST["uID"]; verknuepfung zur user tabelle, erstellender user automatsich einfügen!?
+include('dbcon.php');
+// Variablen um die Werte zwischen zu speichern, die vom
+$eName = $_POST["ename"]; //Frontend übergeben werden
+$euId = 9 ;// $_POST["uID"]; verknuepfung zur user tabelle, erstellender user automatsich einfügen!?
 $eDesc = $_POST["des"];
 $eLoc = $_POST["loc"];
-$erating = $_POST["rating"];
-$ecreationtime = $_POST["ecretime"];
-$edate = $_POST["edate"]
-$eduration = $_POST["dura"];
-$membCap = $_POST["memberC"];
+$erating = null;//$_POST["rating"];
+$ecreationtime = null;//$_POST["ecretime"];
+$edate = null;//$_POST["edate"]
+$eduration = null;//$_POST["dura"];
+$membCap = null;//$_POST["memberC"];
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
+// das sql insert bleibt noch zu prüfen, wie wir das mit user id als Fremdschlüssel machen
+//$sql ="INSERT INTO event(eventid, eventname, eventuid, eventloc, eventrtng, eventtme, eventdte, eventdrtn, eventmbr, eventcp)
+//VALUES(null, '$eName','$euId','$eLoc', '$eDesc', '$erating','$ecreationtime', '$edate', '$eduration', null, '$membCap');";
 
-$sql ="INSERT INTO event(eventID, eventname, eventuid, eventloc, eventabt, eventrtng, eventtme, eventdte, eventdrtn, eventtmbr, eventcp)
-VALUES(null,'$eName','$euId','$eLoc', '$eDesc', '$erating','$ecreationtime', '$edate', '$eduration', null, '$membCap');";
-if($conn->query($sql) == true){
-	echo "Event hinzugefügt!";
-} else { "Hat nicht geklappt!" . $conn->error;
-}
-
-?>
+$sql1 ="INSERT INTO event(eventname, eventuid, eventloc, eventrtng, eventtme, eventdte, eventdrtn, eventmbr, eventcp)
+VALUES('$eName',null,null,null, null ,null, null , null, null);";
+if($conn->query($sql1) == true){
