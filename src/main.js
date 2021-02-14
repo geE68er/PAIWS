3650f37f7022396fa086de027aee3296ef451245
rupertgehrlein <75689560+rupertgehrlein@users.noreply.github.com>
2021-02-14 14:15:49 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,11 +6,27 @@ import { fas } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import vuetify from "./plugins/vuetify";
 import DatetimePicker from 'vuetify-datetime-picker';
+import { domain, clientId } from "../auth_config.json";
+import { Auth0Plugin } from "./auth";
 
 library.add(fas);
 Vue.component("font-awesome-icon", FontAwesomeIcon);
 Vue.use(DatetimePicker);
 Vue.config.productionTip = false;
+
+Vue.use(Auth0Plugin, {
+  domain,
+  clientId,
+  onRedirectCallback: appState => {
+    router.push(
+      appState && appState.targetUrl
+        ? appState.targetUrl
+        : window.location.pathname
+    );
+  }
+});
+
+Vue.config.productionTip = false
 
 new Vue({
   router,
