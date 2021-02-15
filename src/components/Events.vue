7e2c361c315076540412f7fead50703df56f908b
rupertgehrlein <75689560+rupertgehrlein@users.noreply.github.com>
2021-02-15 17:58:40 +0100

    Update Events.vue

--- src/components/Events.vue
+++ src/components/Events.vue
@@ -1,101 +1,59 @@
 <template>
-<div>
-  <v-col class="text-right">
-  <v-spacer></v-spacer>
-  <v-btn 
-  fab
-  elevation="5"
-  color="red"
-  x-large
-  ><router-link to="/Createevent" tag="span"><v-icon>mdi-plus</v-icon></router-link>
-  </v-btn>
-  </v-col>
-<v-layout wrap>
-  
-<v-flex v-for="event in events" :key="event.eventid">
-  <v-card
-    class="mx-auto"
-    width="500"
-    outlined
-    elevation="5"
-  >
-    <v-list-item three-line>
-      <v-list-item-content>
-        <div class="overline mb-4">
-          {{event.eventloc}}
-        </div>
-        <v-list-item-title class="headline mb-1">
-         {{event.eventname}}
-        </v-list-item-title>
-        <v-list-item-subtitle>{{event.eventdte}}</v-list-item-subtitle>
-      </v-list-item-content>
-
-      <v-list-item-avatar
-        tile
-        size="80"
-      >
-        
-      </v-list-item-avatar>
-    </v-list-item>
-
-    <v-card-actions>
-      <v-btn
-        icon 
-        @click="event.show = !event.show"
-      >
-        <v-icon>{{ event.show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
-      </v-btn>
-      <v-spacer></v-spacer>
-
-      <v-btn icon v-bind:disabled="hasVoted" >       
-        <v-icon>mdi-thumb-up {{event.eventrtng}}</v-icon>
-      </v-btn>
-
-      <v-btn icon v-bind:disabled="hasVoted" >       
-        <v-icon>mdi-thumb-down</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-bookmark</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-share-variant</v-icon>
-      </v-btn>
-    </v-card-actions>
-
-    <v-expand-transition>
-      <div>
-        <v-divider></v-divider>
-
-        <v-card-text>
-          Max. Teilnehmer: {{event.eventcp}} <br><br>
-          {{event.eventabt}}
-        </v-card-text>
-      </div>
-    </v-expand-transition>
-  </v-card>
-</v-flex>
-</v-layout>
-</div>
+  <b-row>
+    <b-col cols="12">
+      <h2>
+        Events
+        <b-link href="#/createevent">(Event hinzufügen)</b-link>
+      </h2>
+      <b-table striped hover :items="boards" :fields="fields">
+        <template v-slot:cell(actions)="data">
+          <b-button @click.stop="details(data.item)" variant="primary">Details</b-button>
+        </template>
+      </b-table>
+    </b-col>
+  </b-row>
 </template>
 
 <script>
+
+import firebase from '../Firebase'
+import router from '../router'
+
 export default {
-  data() {
+  name: 'BoardList',
+  data () {
     return {
-      events: []
+      fields: [
+        { key: 'title', label: 'Titel', sortable: true, 'class': 'text-left' },
+        { key: 'actions', label: 'Action', 'class': 'text-center' }
+      ],
+      boards: [],
+      errors: [],
+      ref: firebase.firestore().collection('boards'),
     }
   },
-
-  beforeCreate() {
-    fetch("../extremevent.php")
-    .then(response => response.json())
-    .then(data => {
-      this.events = data
-      console.log(data)
-    })
-
+  created () {
+    this.ref.onSnapshot((querySnapshot) => {
+      this.boards = [];
+      querySnapshot.forEach((doc) => {
+        this.boards.push({
+          key: doc.id,
+          title: doc.data().title
+        });
+      });
+    });
+  },
+  methods: {
+    details (board) {
+      router.push({ name: 'ShowBoard', params: { id: board.key }})
+    }
   }
-};
+}
 </script>
+
+<style>
+  .table {
+    width: 96%;
+    margin: 0 auto;
+  }
+</style>
